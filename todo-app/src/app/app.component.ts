c9dd18468adf6bd01b55b733afde5ea0e81a8e24
dewMyth <akalankadewmith@gmail.com>
2020-10-27 20:23:28 +0530

    Added Todo and Todo Item

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular | To DO App';
+  title = 'Angular | To Do App';
+  name = "Dewmith"
 }
